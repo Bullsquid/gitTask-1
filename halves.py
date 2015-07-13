80b1b6b97cffce99d383375edadd8fd323b2477e
Andrew Boyko <andrew.boyko@filmon.com>
2015-07-13 14:23:29 +0300

    rewrite recursion to cycle, show plot

--- halves.py
+++ halves.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 def min_halves(f, a, b, eps):
 
     if b < a:
@@ -5,34 +9,30 @@ def min_halves(f, a, b, eps):
         a = b
         b = tmp
 
-    length = b - a
-    center = (a + b) / 2.0
-    delta = length / 4.0
-    x1 = center - delta
-    x2 = center + delta
+    t = np.arange(a-1.0, b+1.0, 0.02)
+    right = []
+    left = []
+    plt.plot(t, f(t))
+    while b-a >= eps:
+        center = (a + b) / 2.0
+        delta = (b-a) / 4.0
+        x1 = center - delta
+        x2 = center + delta
+        if f(x1) < f(x2):
 
-    if f(x1) < f(x2):
-
-        if (x2 - a) < eps:
-
-            x_min = (x2 + a) / 2.0
-
-            return {"x": x_min,
-                    "f": f(x_min)
-                    }
-
-        else:
-            return min_halves(f, a, x2, eps)
-
-    else:
-        if (b - x1) < eps:
-
-            x_min = (b + x1) / 2.0
-
-            return {"x": x_min,
-                    "f": f(x_min)
-                    }
+            b = x2
+            right.append(x2)
 
         else:
 
-            return min_halves(f, x1, b, eps)
+            a = x1
+            left.append(x1)
+
+    plt.plot(right, map(f, right), 'rs')
+    plt.plot(left, map(f, left), 'bs')
+    plt.show()
+
+    x_min = (a+b) / 2.0
+    return {"x": x_min,
+            "f": f(x_min)
+            }
